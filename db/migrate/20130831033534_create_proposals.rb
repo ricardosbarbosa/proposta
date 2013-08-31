dfad181f59349dd6eb63d38f1a7c5c19a92245be
Ricardo Barbosa <rbrico@gmail.com>
2013-08-31 00:44:43 -0300

    Add a down method to migration.

--- db/migrate/20130831033534_create_proposals.rb
+++ db/migrate/20130831033534_create_proposals.rb
@@ -1,10 +1,14 @@
 class CreateProposals < ActiveRecord::Migration
-  def change
+  def up
     create_table :proposals do |t|
       t.string :title
       t.text :description
 
       t.timestamps
     end
+  end
+
+  def down
+  	drop_table :proposals
   end
 end
