8af6078127a7ab5ac8b03fc9cf9f7d3d0168d65a
Ricardo Barbosa <rbrico@gmail.com>
2013-08-30 23:57:25 -0300

    inicio

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Proposta::Application.config.secret_token = 'e42dcddbce6faa5fe4d4f01c99ce0023b7fdf78049fbd45b7e0479222a950956e460770250fda5661909ed5db73ddeb1d3794bb42c9f83a188f3e3a768ec9595'
